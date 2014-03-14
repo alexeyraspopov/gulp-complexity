e51b1f9f9cb9d44b7370eb805c977ecfbae19062
Avenger7x <avenger7x13@gmail.com>
2014-03-14 21:01:55 +0200

    report template (WIP)

--- index.js
+++ index.js
@@ -2,14 +2,40 @@
 var cr = require('complexity-report'),
 	map = require('map-stream'),
 	gutil = require('gulp-util'),
+	chalk = require('chalk'),
+	multiline = require('multiline'),
 	PluginError = gutil.PluginError;
 
-function log(file, report){
-	console.log(' ');
-	console.log(file.path);
-	console.log('    ', 'maintainability:', report.maintainability.toFixed(2));
-	console.log('    ', 'cyclomatic:', report.aggregate.complexity.cyclomatic);
-	console.log('    ', 'halstead difficulty:', report.aggregate.complexity.halstead.difficulty);
+function log(file, report, options){
+	var template = require('underscore').template;
+
+	var reportTemplate = multiline(function(){/*
+<%= chalk.styles.yellow.open %><%= path %>:<%= line %> - <%= name %> is too complicated<%= chalk.styles.yellow.close %>
+    Cyclomatic: <%= complexity.cyclomatic %>
+    Halstead: <%= complexity.halstead.difficulty %>
+      | Effort: <%= complexity.halstead.effort %>
+      | Volume: <%= complexity.halstead.volume %>
+      | Vocabulary: <%= complexity.halstead.vocabulary %>
+	*/});
+
+	var reportFn = template(reportTemplate);
+
+	report.functions.filter(function(fn){
+		var cyclomatic = fn.complexity.cyclomatic,
+			halstead = fn.complexity.halstead;
+
+		return cyclomatic > options.cyclomatic[0] || halstead.difficulty > options.halstead[0];
+	}).map(function(fn){
+		return {
+			path: require('path').relative(file.cwd, file.path),
+			line: fn.line,
+			name: fn.name,
+			complexity: fn.complexity,
+			chalk: chalk
+		};
+	}).forEach(function(report){
+		console.log(reportFn(report));
+	});
 }
 
 function complexity(options){
@@ -30,7 +56,7 @@ function complexity(options){
 
 		var report = cr.run(file.contents.toString(), options);
 
-		log(file, report);
+		log(file, report, options);
 
 		cb(null, file);
 	});
